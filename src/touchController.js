0b0a25b6ff898d205f0876fc96986fd5b89b4a94
maxwellito <1097232+maxwellito@users.noreply.github.com>
2021-04-15 21:57:25 +0100

    Add the touch controller

---
+++ src/touchController.js
@@ -0,0 +1,18 @@
+/**
+ * The touch Controller will handle the touch events
+ * on the main canvas to handle the basic gestures
+ * of a graphical editor on a tablet or desktop.
+ *
+ * Things to handle:
+ * - one finger
+ *     tap: nothing
+ *     move: draw
+ * - double fingers
+ *     tap: undo
+ *     move: scale + move
+ * - three fingers
+ *     tap: redo
+ *     move: nothing
+ * - more fingers
+ *     please zoidberg stop licking that screen
+ */
